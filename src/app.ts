2f1a01f4a8850775d9fcdb34470b3115b6fc1def
forestaa <t.daichi.3@gmail.com>
2019-01-08 10:29:52 +0900

    convert require to import

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as Express from "express";
 import * as BodyParser from 'body-parser';
+import * as GoogleHome from './googlehome';
 
 const app = Express();
 app.use(BodyParser.urlencoded({extended: true}));
@@ -8,11 +9,10 @@ const server = app.listen(3000, () => console.log("Node.js is listening to PORT:
 app.set('view engine', 'ejs');
 app.get('/', (req, res, next) => res.render('index', {}));
 
-const googlehome = require('./googlehome')
 app.post('/music', (req, res) => {
   console.log('post')
   try {
-    googlehome.playMusic(req.body.url);
+    GoogleHome.playMusic(req.body.url);
     res.render('index', {}); //TODO: return appropriate html
   } catch (err) {
     console.log('error');
