60f605fb6e40f51b9ff124fbd9f9f631868ec764
forestaa <t.daichi.3@gmail.com>
2019-01-30 10:44:29 +0900

    play playlist

--- src/typescript/googlehome.js
+++ src/typescript/googlehome.js
@@ -1,15 +1,17 @@
-const googlehome = require('google-home-notifier');
-const language = 'jp';
-var deviceAddress = '';
+import { spawn } from 'child_process';
+
 var mdns = require('multicast-dns')();
+var Client = require('castv2-client').Client;
+var DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
+
+var host = '';
 mdns.on('response', function (response) {
     let name = '';
     for (const additional of response.additionals) {
         if (additional.type == 'TXT') name = additional.name;
-        if (additional.type == 'A') deviceAddress = additional.data;
+        if (additional.type == 'A') host = additional.data;
     }
-    if (name != '') console.log(`${name}(${deviceAddress})`);
-    googlehome.ip(deviceAddress, language);
+    if (name != '') console.log(`${name}(${host})`);
     mdns.destroy();
 });
 
@@ -25,17 +27,68 @@ setTimeout(function() {
 }, 2500);
 
 
-const ytdl = require('ytdl-core');
-export function playMusic(url) {
-  console.log(`ipaddress = ${deviceAddress}`); // TODO: check whether the address is valid
-  ytdl.getInfo(url, (err, info) => {
-    if (err) throw err; // TODO: report appropriate error 
-    let audioFormat = ytdl.filterFormats(info.formats, 'audioonly');
-    if (audioFormat.length > 0) { // TODO: report no audio error
-      let url = audioFormat.pop().url;
-      googlehome.play(url, res => {
-        console.log(res); // TODO: report appropriate error
+export async function playMusic(url) {
+  console.log(`ipaddress = ${host}`); // TODO: check whether the address is valid
+
+  const infos = await getInfos(url);
+  const items = infos.map((info) => { 
+    return {media: {
+      contentId: info.url,
+      streamType: 'BUFFERED',
+      metadata: {
+        metadataType: 3,
+        title: info.title,
+        images: [{url: info.thumbnail}],
+      }
+    }}; 
+  });
+  const client = new Client();
+  client.connect(host, () => {
+    console.log('connected! Launching DefaultMediaReceiver ...');
+    client.launch(DefaultMediaReceiver, (err, player) => {
+      if(err) throw err; 
+      console.log('app "%s" launched, loading media ...', player.session.displayName);
+      player.on('status', function(status) {
+        console.log('status broadcast playerState=%s', status.playerState);
       });
+      
+      player.queueLoad(items, {}, (err, status) => {
+        if (err) throw err;
+        console.log(status)
+      });
+    });
+  });
+  client.on('error', function(err) {
+    client.close();
+    throw err;
+  });
+}
+
+async function getInfos(url) {
+  const youtubedl = spawn('youtube-dl', ['--get-title', '--get-url', '--get-thumbnail', '-x', url]);
+  var n = 0;
+  var info = {};
+  var infos = [];
+  for await (const data of youtubedl.stdout) {
+    console.log(n + ': ' + data);
+    const lines = data.toString().split('\n').filter((line) => line != "");
+    for (const line of lines) {
+      switch (n % 3) {
+        case 0:
+          info.title = line;
+          break;
+        case 1:
+          info.url = line;
+          break;
+        default:
+          info.thumbnail = line;
+          infos.push(info);
+          info = {};
+          break;
+      }
+      n += 1;
     }
-  }); 
+  }
+  console.log(infos);
+  return infos;
 }
